bf7f4af308a6069d439f3f03958980a1ac7326ee
Prasanta Kr Dutta <pkd.dapacific@gmail.com>
2017-02-22 04:19:15 +0530

    on screen ripple added

--- js/main.js
+++ js/main.js
@@ -23,6 +23,14 @@
   	})
 
 
+  $(document).ready(function() {
+  $(".rippler").rippler({
+    effectClass      :  'rippler-effect'
+    ,effectSize      :  16      // Default size (width & height)
+    ,addElement      :  'div'   // e.g. 'svg'(feature)
+    ,duration        :  400
+  });
+});
   	/*---------------------------------------------------- */
 	/* FitVids
 	------------------------------------------------------ */ 
