1a57c0fe9b655e81ff598af7655882988f83d4f6
holy-devil <rjddeshmukh@gmail.com>
2017-02-24 20:46:57 +0530

    fixing the close of the menu
    Fixed the close of the menu

--- js/main.js
+++ js/main.js
@@ -293,13 +293,15 @@ if($(window).width()>768) {
         	
             $(document).delegate('.open', 'click', function(event){
 				$(this).addClass('oppenned');
+				$(this).attr('id','close');
 				event.stopPropagation();
 			})
 			$(document).delegate('body', 'click', function(event) {
 				$('.open').removeClass('oppenned');
 			})
-			$(document).delegate('.cls', 'click', function(event){
+			$(document).delegate('#close', 'click', function(event){
 				$('.open').removeClass('oppenned');
+				$('.open').removeAttr('id');
 				event.stopPropagation();
 			});
         });
